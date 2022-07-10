4ef47d2015a9c860e26790147937361be1abc659
AlexDeLara <hfdelara@gmail.com>
2022-07-10 13:14:52 -0500

    CREATE: Added js_example.js and moved css_examples.css to new folder examples

---
+++ examples/js_example.js
@@ -0,0 +1,26 @@
+// Variables
+var x = 3;
+var image = new Image("image.png")              //new: Make an object
+
+// Functions
+function square(x) {
+    var ans = x * x;
+    return ans;
+}
+
+var y = square(4);
+
+// Loops
+for (var pixel of img.values()) {
+    var newG = 255 - pixel.getGreen();
+    pixel.setGreen(newG);
+}
+
+// Conditionals
+if (x , y) {
+    z = 2;
+}
+else {
+    a = y + 1;
+    y = x - 3;
+}
