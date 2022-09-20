f74b9f84c64a72b01ec49fdc66f31655e8ea1188
AlexDeLara <hfdelara@gmail.com>
2022-09-19 20:19:34 -0500

    Updated react-redux example

--- examples/react-redux.jsx
+++ examples/react-redux.jsx
@@ -1,3 +1,31 @@
+/*
+HOW TO INSTALL CREATE REACT APP: https://www.freecodecamp.org/news/install-react-with-create-react-app/
+
+
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { Provider, connect } from 'react-redux'
+import { createStore, combineReducers, applyMiddleware } from 'redux'
+import thunk from 'redux-thunk'
+
+import rootReducer from './redux/reducers'
+import App from './components/App'
+
+const store = createStore(
+  rootReducer,
+  applyMiddleware(thunk)
+);
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App/>
+  </Provider>,
+  document.getElementById('root')
+);
+*/
+
+// Only change code below this line
+
 {/*
 React Redux provides a small API with two key features: Provider and connect. 
 The Provider is a wrapper component from React Redux that wraps your React app. 
